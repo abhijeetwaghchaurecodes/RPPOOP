9db42b12f8ee55efdc9572b5b9bf688bf1b6e087
abhijeetwaghchaurecodes <99084548+abhijeetwaghchaurecodes@users.noreply.github.com>
2023-03-21 10:04:54 +0530

    Create assignment2(polymorphism).py

---
+++ assignment2(polymorphism).py
@@ -0,0 +1,15 @@
+"""
+Polymorphism is a concept in object-oriented programming that refers to the ability of an object or function to take on different forms or behaviors 
+depending on the context in which it is used. In other words, it allows different objects to respond to the same message or method call in different ways.
+
+There are two main types of polymorphism in OOP: runtime polymorphism and compile-time polymorphism.
+
+[1] Runtime Polymorphism:
+    Runtime polymorphism is also known as dynamic polymorphism. It occurs when a method or function is called at runtime, and the actual implementation of the method 
+    or function is determined based on the object that is calling it.
+    
+[2] Compile-time Polymorphism:
+    Compile-time polymorphism is also known as static polymorphism. It occurs when the type of an object or function is determined at compile-time rather 
+    than runtime. This type of polymorphism is achieved through function overloading, which is a technique that allows a function to have multiple 
+    implementations with different parameters.
+"""
