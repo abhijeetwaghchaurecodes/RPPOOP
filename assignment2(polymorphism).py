f7a045e23328ce4285e4d93acc38f0951a263176
abhijeetwaghchaurecodes <99084548+abhijeetwaghchaurecodes@users.noreply.github.com>
2023-03-28 10:02:40 +0530

    Update assignment2(polymorphism).py

--- assignment2(polymorphism).py
+++ assignment2(polymorphism).py
@@ -13,3 +13,50 @@ There are two main types of polymorphism in OOP: runtime polymorphism and compil
     than runtime. This type of polymorphism is achieved through function overloading, which is a technique that allows a function to have multiple 
     implementations with different parameters.
 """
+
+#RUN-TIME POLYMORPHISM
+class Animal:
+    def __init__(self, name):
+        self.name = name
+    
+    def sound(self):
+        pass
+
+class Dog(Animal):
+    def __init__(self, name):
+        super().__init__(name)
+        
+    def sound(self):
+        return "Woof"
+
+class Cat(Animal):
+    def __init__(self, name):
+        super().__init__(name)
+        
+    def sound(self):
+        return "Meow"
+
+def make_sound(animal):
+    print(animal.sound())
+
+dog = Dog("Rufus")
+cat = Cat("Whiskers")
+
+make_sound(dog) # Output: Woof
+make_sound(cat) # Output: Meow
+
+
+
+#COMPILE-TIME POLYMORPHISM
+class Calculator:
+    def add(self, a, b):
+        return a + b
+    
+    def add(self, a, b, c):
+        return a + b + c
+
+calc = Calculator()
+
+print(calc.add(1, 2)) # Output: TypeError: add() missing 1 required positional argument: 'c'
+print(calc.add(1, 2, 3)) # Output: 6
+
